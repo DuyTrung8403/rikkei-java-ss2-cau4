15692be790d364bd6a36207633e18b88597f9d54
Nguyễn Duy Trung <trung08042003@gmail.com>
2026-06-27 09:04:29 +0700

    update

--- src/Main.java
+++ src/Main.java
@@ -13,15 +13,15 @@ public class Main {
             b = Integer.parseInt(sc.nextLine());
             System.out.print("Nhap canh 3 ");
             c = Integer.parseInt(sc.nextLine());
-            isTriangle = a+b>c && b+a>c && a+c>b;
+            isTriangle = (a + b > c) && (b + c > a) && (a + c > b);
             if (!isTriangle) {
-                System.out.println("Lỗi: 3 số vừa nhập không hợp lệ. Vui lòng nhập lại!\n");
+                System.out.println("Ba cạnh k tạo thành tam giác");
             }
         } while (!isTriangle);
         if(a == b && b == c ){
             System.out.println("Day la tam giac deu");
         }else if (a == b || b ==c || c == a){
-            System.out.println("Day la tam giac deu");
+            System.out.println("Day la tam giac can");
         }else if (a * a + b * b == c * c || a * a + c * c == b * b || b * b + c * c == a * a) {
             System.out.println("Day la tam giac vuong");
         }else {
